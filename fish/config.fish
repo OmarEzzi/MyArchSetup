215a2e8ecd557da79192d6785e78cf32a6d81601
OmarEzzi <omar.h.ezzi@proton.me>
2026-08-29 14:32:03 +0300

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias ls='ls --color=auto'
+
+alias c 'clear'
+alias ll 'ls -lah --color=auto'
+alias .. 'cd ..'
+
+alias s 'pacman -Ss'
+alias i 'sudo pacman -S'
+alias r 'sudo pacman -Rns'
+alias cls 'sudo pacman -Sc'
+
+
+alias iy='yay -S'
+alias u='yay -Syu'
+
+alias po='systemctl poweroff'
+alias rb='systemctl reboot'
+alias lo='swaymsg exit'
+alias x='exit'
+
+alias nm='nmtui'
